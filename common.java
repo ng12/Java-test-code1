e509651b4ad05d351844011267e91170828e3910
ng12 <gupta.sweet.niti@gmail.com>
2015-10-12 17:23:24 +0530

    Common.java
    To find the common numbers in two arrays

---
+++ common.java
@@ -0,0 +1,16 @@
+public class Common{
+
+public static void main (String args[]){
+
+int a1 [] = {1,2,3,4,35,56,7,67};
+int a2 [] = {1,2,3,4,3,5645,7,768,78,767};
+for (int i=0 ; i<a1.length ; i++){
+  for(int j =0;j<=a2.length();j++){
+   if(a1[i] == a2[j]){
+    System.out.println("Common numbers are :" a1[i]);
+   }  
+  }
+}
+}
+
+}
